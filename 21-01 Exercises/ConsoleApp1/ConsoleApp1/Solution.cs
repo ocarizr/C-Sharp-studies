7683546b512522153fba444311e8fc42c8853cc4
ocarizr <rafael.ocariz90@gmail.com>
2019-01-22 11:20:58 -0200

    21-01 Exercises
    Exercises update

---
+++ 21-01 Exercises/ConsoleApp1/ConsoleApp1/Solution.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Solution
+    {
+        public int solution (int X, int[] A)
+        {
+            int iSolution = 0;
+            int[] x = new int[X];
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                x[A[i] - 1] = 1;
+
+                for (int j = 0; j < x.Length; j++)
+                {
+                    iSolution += x[j];
+                }
+                if (iSolution == X)
+                {
+                    return i;
+                }
+                iSolution = 0;
+            }
+            return -1;
+        }
+
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            Console.Write("The Number of Kilometers you want to be paved: ");
+            int X = int.Parse(Console.ReadLine());
+            Console.Write("Now fill the number of months you want to schedule the work: ");
+            int month = int.Parse(Console.ReadLine());
+            int[] A = new int[month];
+            for (int i = 0; i < month; i++)
+            {
+                Console.Write("Month number " + (i) + ": ");
+                A[i] = int.Parse(Console.ReadLine());
+                if (A[i] > X || A[i] < 1)
+                {
+                    i--;
+                }
+            }
+
+            Console.WriteLine("Calculating the earliest month when the road will be completely paved...");
+            Console.WriteLine("The road will be completely paved in month " + s.solution(X, A));
+
+            Console.Read();
+        }
+    }
+}
---
+++ 21-01 Exercises/ConsoleApp2/ConsoleApp2/Solution.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Solution
+    {
+        public int solution(int[] A)
+        {
+            int crossingPlanes = 0;
+
+            for (int i = 0; i < A.Length - 1; i++)
+            {
+                if ((A[i]) == 0)
+                {
+                    for (int j = i + 1; j < A.Length; j++)
+                    {
+                        if (A[j] == 1)
+                        {
+                            crossingPlanes++;
+                        }
+                    }
+                }
+            }
+
+            if (crossingPlanes > 1000000000)
+            {
+                return -1;
+            }
+
+            return crossingPlanes;
+        }
+
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            Console.Write("In how many parts was the route divided? ");
+            int a = int.Parse(Console.ReadLine());
+            int[] A = new int[a];
+            Console.WriteLine("Now fill with 0 (to NW-L) and 1 (to L-NW) each slice of the road:");
+            for (int i = 0; i < a; i++)
+            {
+                Console.Write("Plane on the part " + i + " of the route: ");
+                A[i] = int.Parse(Console.ReadLine());
+
+                if (A[i] < 0 || A[i] > 1)
+                {
+                    i--;
+                }
+            }
+
+            Console.WriteLine("There are " + s.solution(A) + " pairs of crossing planes");
+
+            Console.Read();
+        }
+    }
+}
