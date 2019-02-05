228bae44247da9bd9a72e1c36c1bbebab08df7d6
ocarizr <rafael.ocariz90@gmail.com>
2019-02-05 11:23:17 -0200

    New exercises
    Exercises from the Udemy Course

---
+++ Udemy Exercises/CursoCSharp-P1/CursoCSharp-P1/ClassExercise.cs
@@ -0,0 +1,39 @@
+﻿namespace CursoCSharp_P1
+{
+    class ClassExercise
+    {
+        public bool ConditionResult = false;
+
+        public void MinorThanTen(int a) // return if a is minor than 10
+        {
+            if (a >= 10)
+            {
+                ConditionResult = false;
+                return;
+            }
+
+            ConditionResult = true;
+        }
+
+        public void Greater(int a, int b) // return if a is greater than b
+        {
+            if (a < b)
+            {
+                ConditionResult = false;
+                return;
+            }
+
+            ConditionResult = true;
+        }
+
+        public static bool InvertResult(bool a) // Return the oposite of the input (Logic port NOT)
+        {
+            return !a;
+        }
+
+        public override string ToString()
+        {
+            return $"The last result obtained is {ConditionResult}.";
+        }
+    }
+}
---
+++ Udemy Exercises/CursoCSharp-P1/CursoCSharp-P1/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Globalization;
+
+namespace CursoCSharp_P1
+{
+    class Program
+    {
+        public void WaitAndClear()
+        {
+            Console.ReadLine();
+            Console.Clear();
+        }
+
+        public void WriteLineLesson()
+        {
+            string produto1 = "Computador";
+            string produto2 = "Mesa de escritório";
+
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preco1 = 2100.0;
+            double preco2 = 650.5;
+            double medida = 53.234567;
+
+            Console.WriteLine("Produtos:");
+            Console.WriteLine($"{produto1}, cujo preço é R${preco1:F2}");
+            Console.WriteLine($"{produto2}, cujo preço é R${preco2:F2}");
+            Console.WriteLine();
+            Console.WriteLine($"Registro: {idade} anos de idade, código {codigo} e gênero: {genero}");
+            Console.WriteLine();
+            Console.WriteLine($"Medida com oito casas decimais: {medida:F8}");
+            Console.WriteLine($"Arredondado (três casas decimais): {medida:F3}");
+            Console.WriteLine($"Separador decimal invariant culture: {medida.ToString("F3", CultureInfo.InvariantCulture)}");
+        }
+
+        public void ReadLineLesson()
+        {
+            Console.WriteLine("Entre com seu nome completo");
+            string nomeCompleto = Console.ReadLine();
+            Console.WriteLine("Quantos anos você tem:");
+            int idade = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o preço de um produto:");
+            float preco = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Entre seu último nome, idade e altura (mesma linha):");
+            string[] personalData = Console.ReadLine().Split(' ');
+
+            string personalLastName = personalData[0];
+            int personalAge = int.Parse(personalData[1]);
+            float personalHeight = float.Parse(personalData[2], CultureInfo.InvariantCulture);
+
+            Console.WriteLine(nomeCompleto);
+            Console.WriteLine(idade);
+            Console.WriteLine(preco.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(personalLastName);
+            Console.WriteLine(personalAge);
+            Console.WriteLine(personalHeight.ToString("F2", CultureInfo.InvariantCulture));
+        }
+
+        public void FunctionLesson()
+        {
+            Console.WriteLine("Digite três números:");
+            int a = int.Parse(Console.ReadLine()),
+                b = int.Parse(Console.ReadLine()),
+                c = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Maior = {Maior(a, b, c)}");
+        }
+        
+        public int Maior(int a, int b, int c)
+        {
+            if (a > b && a > c)
+            {
+                return a;
+            }
+            else if (b > c)
+            {
+                return b;
+            }
+            else
+            {
+                return c;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var p = new Program();
+            var c = new ClassExercise();
+            
+            p.WriteLineLesson();
+
+            p.WaitAndClear();
+
+            p.ReadLineLesson();
+
+            p.WaitAndClear();
+            
+            p.FunctionLesson();
+
+            p.WaitAndClear();
+            
+            c.MinorThanTen(9);
+            Console.WriteLine(c);
+            c.MinorThanTen(11);
+            Console.WriteLine(c);
+            c.Greater(1, 2);
+            Console.WriteLine(c);
+            c.Greater(2, 1);
+            Console.WriteLine(c);
+
+            c.ConditionResult = ClassExercise.InvertResult(c.ConditionResult);
+            Console.WriteLine(c);
+        }
+    }
+}
