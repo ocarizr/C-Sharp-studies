7a8b73a9cb6caec77ba959a11c1990581da02d19
ocarizr <rafael.ocariz90@gmail.com>
2019-02-05 23:53:09 -0200

    New Exercise update (CursoCSharp-P2 project)

---
+++ Udemy Exercises/CursoCSharp-P1/CursoCSharp-P2/AccountData.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+
+namespace CursoCSharp_P2
+{
+    class AccountData
+    {
+        public string Name { get; private set; }
+        public int AccountNumber { get; private set; }
+        public double Budget { get; private set; }
+
+        public AccountData(string name, int accountNumber)
+        {
+            Name = name;
+            AccountNumber = accountNumber;
+        }
+
+        public AccountData(string name, int accountNumber, double budget) : this(name, accountNumber)
+        {
+            Budget = budget;
+        }
+
+        public void Deposit(double value)
+        {
+            Budget += value;
+        }
+
+        public void Withdraw(double value)
+        {
+            Budget -= (value + 5.0);
+        }
+
+        public override string ToString()
+        {
+            return $"Account {AccountNumber}, Titular: {Name}, Budget: $ {Budget.ToString("F2", CultureInfo.InvariantCulture)}.";
+        }
+    }
+}
---
+++ Udemy Exercises/CursoCSharp-P1/CursoCSharp-P2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Globalization;
+
+namespace CursoCSharp_P2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AccountData ad;
+            Console.Write("Enter with the account number: ");
+            int accNum = int.Parse(Console.ReadLine());
+            Console.Write("Enter with the Titular name: ");
+            string name = Console.ReadLine();
+            bool rightChoice = false;
+            double budget = 0.0;
+
+            while (rightChoice == false)
+            {
+                Console.Write("There's going to have an initial deposit? (y/n): ");
+                char deposit = char.Parse(Console.ReadLine());
+
+                if (deposit == 'y' || deposit == 'Y')
+                {
+                    Console.Write("Enter with the ammount to deposit: ");
+                    budget = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    rightChoice = true;
+                }
+                else if (deposit == 'n' || deposit == 'N')
+                {
+                    rightChoice = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid option.");
+                }
+            }
+
+            if (budget == 0.0)
+            {
+                ad = new AccountData(name, accNum);
+            }
+            else
+            {
+                ad = new AccountData(name, accNum, budget);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Account Data:");
+            Console.WriteLine(ad);
+
+            Console.WriteLine();
+
+            Console.Write("Enter a deposit value: ");
+            double depositValue = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            ad.Deposit(depositValue);
+            Console.WriteLine(ad);
+
+            Console.WriteLine();
+
+            Console.Write("Enter a withdraw value: ");
+            double withdrawValue = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            ad.Withdraw(withdrawValue);
+            Console.WriteLine(ad);
+        }
+    }
+}
