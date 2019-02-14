c005ccd5c0e3e3d580eff73ec7b02a8b0e525ea0
ocarizr <rafael.ocariz90@gmail.com>
2019-02-13 23:55:35 -0200

    created the project for the next exercise

---
+++ Udemy Exercises/CursoCSharp-P1/CursoCSharp-P8/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CursoCSharp_P8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
