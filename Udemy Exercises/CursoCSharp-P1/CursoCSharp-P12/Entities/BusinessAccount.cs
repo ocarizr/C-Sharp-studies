e956e568eb070e9096ff4d2a61a29763d4d8e6ee
ocarizr <rafael.ocariz90@gmail.com>
2019-02-28 16:53:37 -0300

    Heritage exercises

---
+++ Udemy Exercises/CursoCSharp-P1/CursoCSharp-P12/Entities/BusinessAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CursoCSharp_P12.Entities
+{
+    class BusinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount() { }
+
+        public BusinessAccount(int number, string holder, double balance, double loanLimit)
+            : base(number, holder, balance)
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void GetLoan(double value)
+        {
+            if (value <= LoanLimit)
+            {
+                Balance += value;
+                LoanLimit -= value;
+                return;
+            }
+
+            Console.WriteLine("You don have enought limit to take this loan.");
+        }
+    }
+}
---
+++ Udemy Exercises/CursoCSharp-P1/CursoCSharp-P12/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using CursoCSharp_P12.Entities;
+
+namespace CursoCSharp_P12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Account a = new BusinessAccount(1074, "Monica", 1000, 300);
+            Account b = new SavingsAccount(1388, "Giovanna", 1500, 1.5);
+
+            if (a is BusinessAccount)
+            {
+                a.Deposit(100);
+            } else if (a is SavingsAccount)
+            {
+                a.Withdraw(100);
+            }
+
+            Console.WriteLine(a.Balance);
+
+            if (b is BusinessAccount)
+            {
+                b.Withdraw(100);
+            } else if (b is SavingsAccount)
+            {
+                ((SavingsAccount)b).UpdateBalance();
+            }
+
+            Console.WriteLine(b.Balance);
+
+            BusinessAccount c = new BusinessAccount();
+
+            if (a is BusinessAccount)
+            {
+                c = (BusinessAccount)a;
+            }
+            else if (b is BusinessAccount)
+            {
+                c = b as BusinessAccount;
+            }
+
+            Console.WriteLine(c.LoanLimit);
+
+            c.GetLoan(100);
+
+            Console.WriteLine(c.Balance);
+            Console.WriteLine(c.LoanLimit);
+        }
+    }
+}
